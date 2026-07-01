1ae802faa946fd758cc7c380b8c789e08be9a5b7
Windows 11 <techit.an@dione.zone>
2026-07-01 08:34:43 +0700

    feat: complete application.properties setup for render and netlify

--- warehouse-api/src/main/java/com/example/warehouse/config/SecurityConfig.java
+++ warehouse-api/src/main/java/com/example/warehouse/config/SecurityConfig.java
@@ -71,7 +71,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+    "http://localhost:5173", 
+    "https://my-warehouse-frontend.netlify.app"
+));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
